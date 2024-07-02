e11996e96cb5a48e10f88c6ba25dbb50ca2a35a9
Chandran98 <chandranjc25@gmail.com>
2024-07-02 12:14:58 +0530

    build file

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,9 +1,6 @@
 "use client";
-import React from "react";
-import { cookies } from "next/headers";
 
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 import isUserAuthenticated from "@/components/isAuth";
 
 const middleware = (request) => {
